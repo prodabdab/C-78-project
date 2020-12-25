c1cc224d23eeaae3c8a6efaea07684ae2f07a8f1
prodabdab <73546028+prodabdab@users.noreply.github.com>
2020-12-25 23:33:41 +0530

    Add files via upload

---
+++ next.js
@@ -0,0 +1,13 @@
+var namee=["Hiremath","Saraswathi","Rajshaker"]
+var nnnamme=["https://web.whatsapp.com/a42817ff-f902-4b9f-8ee2-d0e025f9f949","https://web.whatsapp.com/086a353c-db8c-4c3c-b030-d3437bf3ac76","https://web.whatsapp.com/39dad2ca-e0ee-472d-8562-d0ac7d5469ae"]
+
+function justcomboi() {
+    var i = 0;
+    document.getElementById("immmmmfmgmgm").innerHTML=namee[i];
+    document.getElementById("name").src=nnnamme[i];
+    i++;
+    if (i == 2) {
+        i = 0;
+    }
+
+}
